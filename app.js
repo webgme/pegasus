89462ac610be4c7b0314406224f63893e7e9d565
kecso <tkecskes@isis.vanderbilt.edu>
2014-04-04 16:42:59 -0500

    initial setup

---
+++ app.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Zsolt on 3/17/14.
+ */
+
+var config = require('./config.json'),
+    webgme = require('webgme');
+
+
+var myServer = new webgme.standaloneServer(config);
+myServer.start();
